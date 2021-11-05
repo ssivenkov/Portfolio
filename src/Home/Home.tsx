ca845b373f3f17f57edfca04c9994c2c54a4ff70
ssivenkov <ser.sivenkov@gmail.com>
2021-11-05 18:43:31 +0300

    fix contacts error message displaying, add parallax effect for home section with React-parallax

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import s from "./Home.module.scss"
 import Avatar from "../assets/images/avatar.jpg";
+import BackgroundImage from "../assets/images/bg.jpg";
+import { Parallax } from "react-parallax";
 
 export const Home = () => {
     return (
-        <div id={"home"} className={s.generalContainer}>
-            <section className={s.contentContainer}>
-                <img className={s.photo}
-                     src={Avatar}
-                     alt=""/>
-                <h1 className={s.firstLine}>Hi, I'm Sergei Sivenkov</h1>
-                <h2 className={s.secondLine}>Frontend Developer</h2>
-                <div className={s.marginBlock}></div>
-            </section>
+        <div id={"home"}>
+            <Parallax
+                bgImage={BackgroundImage}
+                strength={500}
+                bgImageStyle={{width: "100vw", height: "100vh", objectFit: "cover"}}
+            >
+                <div className={s.generalContainer}>
+                    <section className={s.contentContainer}>
+                        <img className={s.photo}
+                             src={Avatar}
+                             alt=""/>
+                        <h1 className={s.firstLine}>Hi, I'm Sergei Sivenkov</h1>
+                        <h2 className={s.secondLine}>Frontend Developer</h2>
+                        <div className={s.marginBlock}></div>
+                    </section>
+                </div>
+            </Parallax>
         </div>
     )
 }
