b93c885ff741107cb1aae177e678963c83a32b3c
ssivenkov <ser.sivenkov@gmail.com>
2021-11-26 09:21:59 +0300

    update Social network project

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -15,8 +15,8 @@ export const Projects = () => {
                 <Title title={"Projects"}/>
                 <div className={s.projectsContainer}>
                     <Project title={"Social network"}
-                             description={"SPA for social actions - messaging and blogging. Implemented: login page with validation, logout button, profile page with the ability to view the information of the selected user, add a new post and view published posts, message page with the ability to send a message with validation, users page with the ability to view the list of users using pagination, subscribe and unsubscribe to any user, view the user profile."}
-                             technologies={"SCSS, JavaScript, TypeScript, React, Redux, Redux-form, Axios"}
+                             description={"SPA for social actions - messaging and blogging. Implemented: login page with validation, error displaying and captcha; logout buttons, 404 page, profile page with: the ability to view the information of the selected user, edit your profile information, change avatar, add a new post and view your published posts; dialogs page with the ability to send a message with validation; users page and friends page with the ability to view the list of users using pagination, subscribe and unsubscribe to any user, view the user profile."}
+                             technologies={"SCSS, JavaScript, TypeScript, React, Redux, Redux-form, Axios, Reselect"}
                              imageLink={Social_network}
                              link={"https://ssivenkov.github.io/Social_network"}
                     />
