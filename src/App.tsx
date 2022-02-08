4e846255b22b1fb574a7d841024a5ff7503cac51
ssivenkov <ser.sivenkov@gmail.com>
2022-02-08 14:59:31 +0300

    add SimpleBar scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./Null.scss";
+import 'simplebar/dist/simplebar.css';
 import "./App.scss";
 import { Nav } from "./Nav/Nav";
 import { Home } from "./Home/Home";
 import { Skills } from "./Skills/Skills";
 import { Projects } from "./Projects/Projects";
 import { Contacts } from "./Contacts/Contacts";
+import SimpleBar from "simplebar-react";
 
 function App() {
     return (
-        <div>
-            <Nav/>
-            <Home/>
-            <Skills/>
-            <Projects/>
-            <Contacts/>
-        </div>
+        <SimpleBar style={{ maxHeight: "100vh" }}>
+            <div>
+                <Nav/>
+                <Home/>
+                <Skills/>
+                <Projects/>
+                <Contacts/>
+            </div>
+        </SimpleBar>
     )
 }
 
