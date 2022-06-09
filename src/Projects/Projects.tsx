94ed43f486eddd80e56f026c4d6635130d48f590
ssivenkov <ser.sivenkov@gmail.com>
2022-06-09 21:23:38 +0300

    update project description (ToDo List React Native)

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -17,7 +17,7 @@ export const Projects = () => {
                 <Title title={"Projects"}/>
                 <div className={s.projectsContainer}>
                     <Project title={"ToDo List"}
-                             description={"Mobile task management application for Android and iOS. Implemented: add task or task list, remove task or task list, rename task or task list, marking the completed task, screens for todo tasks and for done tasks, receiving notifications at a certain date and time for tasks, sign in with Google, internet connection checking, app accent color picker button, delete account button, change app language buttons, user data storage and synchronizing with Firebase Realtime Database, dark theme mode, splash screen."}
+                             description={"Mobile task management application for Android and iOS. Implemented: add task or task list, remove task or task list, rename task or task list, marking the completed task, mark task with any color, screens for todo tasks and for done tasks, receiving notifications at a certain date and time for tasks, sign in with Google, internet connection checking, app accent color picker button, delete account button, change app language buttons, user data storage and synchronizing with Firebase Realtime Database, dark theme mode, splash screen."}
                              technologies={"JavaScript, TypeScript, React Native, Redux, Redux-Saga, ESLint, Prettier, Firebase, i18next, Font Awesome"}
                              imageLink={Temp}
                              link={"https://github.com/ssivenkov/ToDoList-React-Native"}
