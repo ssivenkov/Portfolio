bcfda8f9866d2440c8a671f39327dab456e09e98
ssivenkov <ser.sivenkov@gmail.com>
2022-09-18 12:36:53 +0300

    add image viewer

--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+
+import ImageViewer from 'react-simple-image-viewer';
 
 import styles from './Project.module.scss';
 import { ProjectPropsType } from './types';
@@ -6,9 +8,37 @@ import { ProjectPropsType } from './types';
 export const Project = (props: ProjectPropsType) => {
   const { title, description, technologies, imageLink, projectLink } = props;
 
+  const [isViewerOpen, setIsViewerOpen] = useState<boolean>(false);
+
+  const openImageViewer = useCallback(() => {
+    setIsViewerOpen(true);
+  }, []);
+
+  const closeImageViewer = () => {
+    setIsViewerOpen(false);
+  };
+
   return (
     <div className={styles.generalContainer}>
-      <img alt={`${title} project`} className={styles.image} src={imageLink} />
+      {isViewerOpen && (
+        <div className={styles.imageViewerContainer}>
+          <ImageViewer
+            backgroundStyle={{ backgroundColor: 'var(--background)' }}
+            closeOnClickOutside
+            currentIndex={0}
+            onClose={closeImageViewer}
+            src={[imageLink]}
+          />
+        </div>
+      )}
+      <div
+        onClick={openImageViewer}
+        onKeyDown={openImageViewer}
+        role='button'
+        tabIndex={0}
+      >
+        <img alt={`${title} project`} className={styles.image} src={imageLink} />
+      </div>
       <div className={styles.descriptionContainer}>
         <h5 className={styles.title}>{title}</h5>
         <span className={styles.descriptionText}>{description}</span>
