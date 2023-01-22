6aa02beff0adf32a87296c53f16a34d36923bb43
ssivenkov <ser.sivenkov@gmail.com>
2023-01-22 12:09:32 +0300

    update ToDoListReactNative project info

--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -1,12 +1,13 @@
 import React, { useCallback, useState } from 'react';
 
+import GetItOnGooglePlay from 'assets/images/other/Google_Play.png';
 import ImageViewer from 'react-simple-image-viewer';
 
 import styles from './Project.module.scss';
 import { ProjectPropsType } from './types';
 
 export const Project = (props: ProjectPropsType) => {
-  const { title, description, technologies, imageLink, projectLink } = props;
+  const { title, description, stack, imageLink, projectLink, googlePlayLink } = props;
 
   const [isViewerOpen, setIsViewerOpen] = useState<boolean>(false);
 
@@ -42,16 +43,32 @@ export const Project = (props: ProjectPropsType) => {
       <div className={styles.descriptionContainer}>
         <h5 className={styles.title}>{title}</h5>
         <span className={styles.descriptionText}>{description}</span>
-        <span className={styles.descriptionText}>{`Technologies: ${technologies}`}</span>
+        <span className={styles.descriptionText}>{`Stack: ${stack}`}</span>
       </div>
-      <a
-        className={styles.buttonTitle}
-        href={projectLink}
-        rel='noreferrer'
-        target='_blank'
-      >
-        View project
-      </a>
+      <div className={styles.buttonsContainer}>
+        {googlePlayLink && (
+          <a
+            className={styles.googlePlayButton}
+            href={googlePlayLink}
+            rel='noreferrer'
+            target='_blank'
+          >
+            <img
+              alt='Google Play link'
+              className={styles.googlePlayImage}
+              src={GetItOnGooglePlay}
+            />
+          </a>
+        )}
+        <a
+          className={styles.viewButton}
+          href={projectLink}
+          rel='noreferrer'
+          target='_blank'
+        >
+          View project
+        </a>
+      </div>
     </div>
   );
 };
